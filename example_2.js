0cb8b868427c52ca9e1cbd5580938e6ecd355057
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-09-01 00:17:09 -0700

    foo

--- example_2.js
+++ example_2.js
@@ -2,9 +2,13 @@ const i2c = require('i2c-bus');
 
 i2c.openPromisified(1)
   .then(sensor => {
-    sensor.readByte(0x6B, 0x0F)
-      .then((data) => {
-        console.log(data)
-        sensor.close()
-      })
+    Promise.all([
+      sensor.readByte(0x6B, 0x0F),
+      sensor.readByte(0x3D, 0x0F)
+    ])
+    .then(([x, y]) => {
+      console.log(x)
+      console.log(y)
+      sensor.close()
+    })
   })
